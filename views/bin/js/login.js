58f5c5a818b074f9cd66f59425d899635f950978
Ckins <492159680@qq.com>
2015-06-14 01:11:46 +0800

    jade

--- views/bin/js/login.js
+++ views/bin/js/login.js
@@ -1,6 +1,19 @@
 (function() {
-  $(function() {
-    return console.log('test');
+  $("#login").click(function(e) {
+    var yy;
+    e.preventDefault();
+    yy = $.post('/log/session', {
+      param1: $('#InputIdCard').value
+    });
+    return yy.done(function(data) {
+      if (data.result) {
+        if (data.result === 'success') {
+          return window.location.replace("/");
+        } else if (data.result === 'fail') {
+          return alert(data.msg);
+        }
+      }
+    });
   });
 
 }).call(this);
--- views/bin/js/register.js
+++ views/bin/js/register.js
@@ -1,6 +1,22 @@
 (function() {
-  $(function() {
-    return console.log('test');
+  $("#register").click(function(e) {
+    var yy;
+    e.preventDefault();
+    yy = $.post('/signup', {
+      param1: $('#InputName').value,
+      param2: $('#InputIdCard').value,
+      param3: $('#InputLocation').value,
+      param4: $('#InputEmail').value
+    });
+    return yy.done(function(data) {
+      if (data.result) {
+        if (data.result === 'success') {
+          return window.location.replace("/");
+        } else if (data.result === 'fail') {
+          return alert(data.msg);
+        }
+      }
+    });
   });
 
 }).call(this);
